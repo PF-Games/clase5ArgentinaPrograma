4b70b0bd948d2205a7e4e62a9459359b4313f1b7
PFGames <132846673+PF-Games@users.noreply.github.com>
2025-01-03 16:50:42 -0300

    resuelve tarea 2 de clase 5

---
+++ tarea-clase-5/tarea2.js
@@ -0,0 +1,18 @@
+//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+
+
+document.querySelector('#entrada-bar').onclick = function(){
+    const edad = Number(document.querySelector('#edad-usuario').value);
+    const nombre = document.querySelector('#nombre-usuario').value;
+ 
+    document.querySelector('#resultado').innerText = `Nombre: ${nombre}, Edad: ${edad}`;
+    document.querySelector('h1').innerText = `¡Hola ${nombre}! ¡Te damos la bienvenida!`;
+
+    return false;
+}
+
